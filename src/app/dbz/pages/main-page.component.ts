55dfab64a278b0f48287b8d7627ae8a065008291
Agustin-19 <ottersttet@gmail.com>
2023-05-11 13:14:41 -0300

    segundo commit

---
+++ src/app/dbz/pages/main-page.component.ts
@@ -0,0 +1,34 @@
+import { Component} from '@angular/core';
+import { Character } from '../interfaces/character.interface';
+
+@Component({
+    selector: 'app-dbz-main-page',
+    templateUrl: 'main-page.component.html'
+})
+
+export class MainPageComponent {
+    constructor() { }
+
+    public characters: Character[] = [
+        {
+            name: 'Krillin',
+            power: 1000
+        },
+        {
+            name: 'Goku',
+            power: 9500
+        },
+        {
+        name: 'Vegetta',
+        power: 7000
+        } 
+    ];
+
+
+    onNewCharacter(character: Character):void{
+        console.log('MainPage');
+        console.log(character);
+        
+        
+    }
+}
